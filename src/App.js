f9e60fdf56f9eb31c07f94ac52fc3378fea3a8d5
Ivan DiLernia <ivan@datadoghq.com>
2020-12-16 15:12:09 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { init } from "@datadog/ui-apps-sdk";
+
+import "./App.css";
+import "typeface-roboto";
+import "milligram";
+import { useEffect, useState } from "react";
+
+let DDClient = null;
+
+function App() {
+  const [name, setName] = useState("Datadog user");
+  const [metric, setMetric] = useState("system.cpu.idle");
+
+  useEffect(() => {
+    DDClient = init({ debug: true }, (c) => {
+      setName(c.appContext.currentUser.handle);
+      setMetric(c.frameContext.options.metric);
+    });
+
+    DDClient.events.on(
+      "dashboard_custom_widget_options_change",
+      ({ metric }) => {
+        setMetric(metric);
+      }
+    );
+  }, []);
+
+  return (
+    <section style={{ padding: "10px" }}>
+      <h2>Hello {name} 👋</h2>
+      <p>Welcome to your first Datadog application.</p>
+      <p>Your favorite metric is: {metric}</p>
+    </section>
+  );
+}
+
+export default App;
